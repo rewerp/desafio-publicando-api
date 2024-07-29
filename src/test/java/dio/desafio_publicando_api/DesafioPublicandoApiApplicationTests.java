7578a3a7beac749cec6405facb3abcd0b7a77c64
rewerp <rewer_p@hotmail.com>
2024-07-29 17:02:37 -0300

    Add: Template de projeto criado pelo Spring Initializr

---
+++ src/test/java/dio/desafio_publicando_api/DesafioPublicandoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.desafio_publicando_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPublicandoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
